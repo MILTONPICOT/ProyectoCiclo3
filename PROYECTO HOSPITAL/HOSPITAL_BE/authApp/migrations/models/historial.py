0d1fc3bfe2a2ac738375ab76d328fffa67ec7664
miltonpicot <miltonpicot@gmail.com>
2022-09-03 10:56:38 -0500

    Milton Picot

---
+++ PROYECTO HOSPITAL/HOSPITAL_BE/authApp/migrations/models/historial.py
@@ -0,0 +1,8 @@
+from django.db import models
+from .user import User
+class Historial(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, related_name='historial', on_delete=models.CASCADE)
+    balance = models.IntegerField(default=0)
+    lastChangeDate = models.DateTimeField()
+    isActive = models.BooleanField(default=True)
